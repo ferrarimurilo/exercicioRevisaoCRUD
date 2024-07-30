2800d8c4a091b3a202c9c736c67c3f9add776f03
Murilo <ferrarimurilo10@gmail.com>
2024-07-30 20:17:53 -0300

    Criado as 4 funções do CRUD

---
+++ cadastro.js
@@ -0,0 +1,37 @@
+const prompt = require("prompt-sync")({sigint:true});
+
+let produtos = [];
+
+let cadastrar = () => {
+    console.log("Cadastrar produtos: ");
+    let nome = prompt("Digite o nome do produto que deseja cadastrar: ");
+    let valor = prompt("Digite o preço do produto que deseja cadastrar: ");
+    produtos.push({nome,valor});
+}
+
+let listar = () => {
+    console.log("Listar produtos: ");
+    for(let i = 0; i < produtos.length; i ++){
+        console.log(`${i+1} - 
+        Produto: ${produtos[i].nome} 
+        Preço: ${produtos[i].valor}
+        `)
+    }
+}
+
+let editar = () => {
+    console.log("Editar produto: ");
+    listar();
+    let opcaoEditar = prompt("Digite o número do produto que deseja editar: ");
+    let nome = prompt("Digite o nome do produto que deseja cadastrar: ");
+    let valor = prompt("Digite o preço do produto que deseja cadastrar: ");
+    produtos[opcaoEditar-1] = {nome, valor};
+
+}
+
+let excluir = () => {
+    console.log("Excluir produto: ");
+    listar();
+    let opcaoExcluir = prompt("Digite o número do produto que deseja excluir: ");
+    produtos.splice(opcaoExcluir-1,1);
+}
